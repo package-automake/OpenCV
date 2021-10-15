da85dbe5ebb20d9164a5ab8a2e6ba6f7c3d7a795
yuzuhakuon <yuzuhakuon@gmail.com>
2021-10-15 15:17:28 +0800

    fix: fix bug

--- fix_opencv_bug.ps1
+++ fix_opencv_bug.ps1
@@ -1,6 +1,9 @@
-function FixOpenCVBug() {
-    $content = Get-Content "opencv_contrib\modules\wechat_qrcode\src\zxing\zxing.hpp"
-    $content[29] = "#if defined(__ANDROID_API__) || defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__)"
-    
-    $content | set-Content "opencv_contrib\modules\wechat_qrcode\src\zxing\zxing.hpp"
+function FixOpenCVBug($version) {
+    $version_list = "4.5.2", "4.5.3", "4.5.4"
+    if ($version_list -contains $version) {
+        $content = Get-Content "opencv_contrib\modules\wechat_qrcode\src\zxing\zxing.hpp"
+        $content[29] = "#if defined(__ANDROID_API__) || defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__)"
+        
+        $content | set-Content "opencv_contrib\modules\wechat_qrcode\src\zxing\zxing.hpp"
+    }
 }
