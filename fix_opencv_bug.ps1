956e69d8c43feaf3b8cb884049cc5201a7b57abb
yuzuhakuon <yuzuhakuon@gmail.com>
2021-10-15 13:41:17 +0800

    fix: fix bug

--- fix_opencv_bug.ps1
+++ fix_opencv_bug.ps1
@@ -1,9 +1,6 @@
 function FixOpenCVBug() {
     $content = Get-Content "opencv_contrib\modules\wechat_qrcode\src\zxing\zxing.hpp"
-    $content[30] = "#if defined(__ANDROID_API__) || defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__)"
+    $content[29] = "#if defined(__ANDROID_API__) || defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__)"
     
-    for ($i = 53; $i -le 62; $i++) {
-        $content[$i] = ""
-    }
     $content | set-Content "opencv_contrib\modules\wechat_qrcode\src\zxing\zxing.hpp"
 }
